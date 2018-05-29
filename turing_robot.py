0edce7fd0a01de84d2642dd9c43cbadb7d1db003
hillium <maruruku@stu.csust.edu.cn>
2018-05-29 20:22:36 +0800

    图灵机器人接口：做了一点微小的贡献

--- turing_robot.py
+++ turing_robot.py
@@ -3,6 +3,7 @@ from configparser import ConfigParser
 from random import randrange
 from sys import stderr
 from functools import partial
+from operator import getitem
 
 errlog = partial(print, file=stderr)
 template = '''{
@@ -20,6 +21,10 @@ template = '''{
     }
 }'''
 
+"""
+机器人类。
+你可以与他进行交互。
+"""
 class turing_robot:
     def __init__(self, userid = None, config_file = 'robotconf.ini'):
         c = ConfigParser()
@@ -38,6 +43,9 @@ class turing_robot:
 
         self.temp = template % ("%d", "%s", self.apikey, self.userid)
     
+    """
+    创建一个文字聊天的 http request。
+    """
     def make_literal_request(self, quesion: str) -> requests.models.Response:
         jsontemp = self.temp % (0, quesion)
         r = requests.post(self.interface_address, data=jsontemp.encode('utf-8'))
@@ -46,16 +54,46 @@ class turing_robot:
         else:
             raise Exception("HTTP request fail with code:", r.status_code)
     
+    """
+    更加简洁的接口：问一句话，获取它的结果（封装于 robot_response 类中）
+    """
     def ask(self, quesion:str):
         r = self.make_literal_request(quesion)
         retjson = r.json()
         results = retjson['results']
-        retstr = ""
-        for res in results:
-            retstr += "{%s}\n" % (res['resultType'])
-            for k, v in res['values'].items():
-                 retstr += "\t[%s]%s\n" % (k, v)
-        r.close()
-        return retstr
+        return robot_response(results)
+
+    """
+    更更加简洁的接口：问一句话，不管返回什么，统统转化为字符串！
+    """
+    def interactive(self, quesion: str) -> str:
+        """
+        没错，这东西现在只能发文字。
+        图片功能我猜可能会在一段时间之后出现。
+        """
+        return self.ask(quesion).get_response_content()
     
-        
+class robot_response:
+    def __init__(self, results):
+        self.grouped_data = []
+        self.data = None
+        for r in results:
+            if r['groupType'] == 0:
+                self.data = (r['resultType'], str.join('\n', r['values'].values()))
+            else:
+                self.grouped_data.append((r['resultType'], str.join('\n', r['values'].values())))
+
+    def get_response_content(self) -> str:
+        if self.data is not None:
+            return self.data[1]
+        else:
+            # 我在这里默认了 groupType 只有 0 或者 1 两种状况。
+            return str.join('\n\n', map(lambda o: getitem(o, 1), self.grouped_data))
+    
+    def get_response(self):
+        if self.data is not None:
+            return self.data
+        else:
+            return self.grouped_data
+
+
