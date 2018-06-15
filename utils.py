c6ed18ba22b49a0edd688aaaaa9c155cff8d924a
hill <maruruku@stu.csust.edu.cn>
2018-06-15 22:17:38 +0800

    一个用来编码字符串的小工具。

---
+++ utils.py
@@ -0,0 +1,31 @@
+from itertools import starmap, cycle, repeat
+from operator import add, mod
+from functools import reduce
+
+
+def url_encode(s: str):
+    """
+    将字符串编码为搜索引擎需求的形式(%XX)。
+    param {s} string to encode.
+    ret the encoded string.
+
+    example
+    > 'flip flappers'
+    > '%66%6c%69%70%20%66%6c%61%70%70%65%72%73'
+
+    > '\01\02'
+    > '%01%02'
+    """
+    encoded = s.encode("utf-8")
+    firstprocess = starmap(add, zip(cycle("%"), 
+                   starmap(mod, zip(repeat("%.2x"), encoded))))
+    return reduce(add, joinl(firstprocess, type("str")))
+    
+
+def joinl(itor, monad = type([])):
+    for x in itor:
+        if type(x) is monad:
+            for y in x:
+                yield y
+        else:
+            yield x
