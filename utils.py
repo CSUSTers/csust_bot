6056c924af374a02acd39207196b6e5cb1be8a17
hill <maruruku@stu.csust.edu.cn>
2018-06-18 22:55:42 +0800

    ....刚才那个 commit 没有加上这个文件

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from itertools import starmap, cycle, repeat
+from itertools import starmap, repeat
 from operator import add, mod
 from functools import reduce
 
@@ -10,14 +10,18 @@ def url_encode(s: str):
     ret the encoded string.
 
     example
-    > 'flip flappers'
+    < 'flip flappers'
     > '%66%6c%69%70%20%66%6c%61%70%70%65%72%73'
 
-    > '\01\02'
+    < '\01\02'
     > '%01%02'
-    """
-    encoded = s.encode("utf-8")
-    firstprocess = starmap(add, zip(cycle("%"), 
-                   starmap(mod, zip(repeat("%.2x"), encoded))))
-    return reduce(add, firstprocess)
+
+    < "来自深渊今天更新了吗？"
+    > "%e6%9d%a5%e8%87%aa%e6%b7%b1%e6%b8%8a%e4%bb%8a%e5%a4%a9%e6%9b%b4%e6%96%b0%e4%ba%86%e5%90%97%ef%bc%9f"
+    """ 
+    return reduce(add,
+                  starmap(
+                      mod, zip(repeat("%%%.2x"),
+                      s.encode("utf-8"))), 
+                  "")
     
