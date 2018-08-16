f327cdc88e1984d4ee11cf15f493eea55288f41e
Hugefiver <hugefiver@outlook.com>
2018-08-16 21:54:09 +0800

    Update letsgo.sh

--- letsgo.sh
+++ letsgo.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-git --depth=1 https://github.com/CSUSTers/csust_bot.git
+git clone --depth=1 https://github.com/CSUSTers/csust_bot.git
 cd csust_bot
 bash easy_start.sh
 pip install python-telegram-bot requests
