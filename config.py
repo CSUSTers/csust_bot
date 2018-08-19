3fb015c5646e1a22564311ce6c8d8b11e8f87e86
hillium <maruruku@stu.csust.edu.cn>
2018-08-19 21:36:06 +0800

    turing robot update.

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
 TOKEN = ''
+
+
+class Config(object):
+    pass
+
+
+class TuringBotConfig(Config):
+    api_address = ""  # interface address here.
+    apikey = ""  # your api key here.
