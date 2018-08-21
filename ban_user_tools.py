3dbc4b9a7ef642919bcbfac80d69591028bf033d
hugefiver <hugefiver@outlook.com>
2018-08-21 16:35:28 +0800

    feature: mv ban_about functions to a single file

---
+++ ban_user_tools.py
@@ -0,0 +1,8 @@
+import datetime
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
+from telegram import error, Bot, Chat, User, Message, ChatMember
+from utils import SecGetter
+import random
+
+class BanUser:
+    pass
