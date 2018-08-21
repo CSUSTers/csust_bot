8e800bd969c43e3f8af82efbe0619db0adc1ba5a
长岛冰茶、 <1373616097@qq.com>
2018-08-21 16:52:19 +0800

    er...

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config(object):
 
 class TuringBotConfig(Config):
     # interface address here.
-    api_address = "http://openapi.tuling123.com/openapi/api/v2"
-    apikey = "538f39210b584fbb8620c4eb7e47a43d"  # your api key here.
+    api_address = ""
+    apikey = ""  # your api key here.
